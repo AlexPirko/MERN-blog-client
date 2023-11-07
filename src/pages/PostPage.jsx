f42ea125ce5184c8470aec11494fc282b38dbc5f
Алекс Южный <youzhny.foto@gmail.com>
2023-11-07 14:14:25 +0200

    add env

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -31,7 +31,7 @@ export const PostPage = () => {
             <Post
                 id={data._id}
                 title={data.title}
-                imageUrl={`http://localhost:4444${data.imageUrl}`}
+                imageUrl={`${process.env.REACT_APP_API_URL}${data.imageUrl}`}
                 user={data.user}
                 createdAt={data.createdAt}
                 viewsCount={data.viewsCount}
